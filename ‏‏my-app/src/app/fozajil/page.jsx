ea693f59b465fa641d0810d1bac850b16d599a18
milad998 <myaralbwd20@gmail.com>
2025-08-26 23:53:10 +0300

    Update page.jsx

--- ‏‏my-app/src/app/fozajil/page.jsx
+++ ‏‏my-app/src/app/fozajil/page.jsx
@@ -35,7 +35,7 @@ const Page = () => {
   const PostToDiscord = async() => {
     const description = Object.entries(form.data)
       .map((d) => `${d[0]} : ${d[1]} `)
-      .join("%0A");
+      .join("\n");
     await fetch("/api/sendMessage", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
